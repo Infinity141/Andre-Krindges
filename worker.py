15decf4991a46db180732afc46f7bbcc673fab5a
Infinity141 <andre.saijo@sou.ufmt.br>
2026-05-04 17:34:50 -0400

    Adicão do do código do Worker e sua inicialização automática

--- worker.py
+++ worker.py
@@ -1,27 +1,43 @@
 import redis
-from rq import Worker, Queue, Connection
+from rq import Worker, Queue
 
 #Defina o ip e a porta do computador do redis
-REDIS_IP = '10.10.10.10'
-REDIS_PORT = 6379
+#REDIS_IP = '10.10.10.10'
+#REDIS_PORT = 6379
+REDIS_URL = 'redis://:liama@10.10.10.10:6379'
+REDIS_IP ='10.10.10.10'
 
+#def iniciar_worker():
+#	#Configura a conexão	
+#	try:
+#		conexao = redis.from_url(REDIS_URL)
+#		
+#		conexao.ping()
+#		#Conecta na fila
+#		with Connection(conexao):
+#			worker = Worker(['operacoes'])
+#			print(f"[*] Conectado ao Redis em {REDIS_IP}")
+#			print(f"[*] Aguardando ordens")
+#			#Começa os trabalhos
+#			#O computador entrará em loop e estará constantemente vendo se há trabalhos
+#			worker.work()
+#	except redis.exceptions.AuthenticationError:
+#		print("Erro: Senha do Redis incorreta.")
+#	except Exception as e:
+#			print(f"[ERRO]: {e}")
 
-def iniciar_worker():
-	#Configura a conexão	
+def iniciar():
 	try:
-		conexao = redis.Redis(host=REDIS_IP, port=REDIS_PORT)
-		
-		#Conecta na fila
-		with Connection(conexao):
-			worker = Worker([operacoes])
-			print(f"[*] Conectado ao Redis em {REDIS_IP}")
-			print(f"[*] Aguardando ordens")
-			
-			#Começa os trabalhos
-			#O computador entrará em loop e estará constantemente vendo se há trabalhos
-			worker.work()
-		except Exception as e:
-			print(f"Erro ao conectar: {e}")
+		conexao_redis = redis.from_url(REDIS_URL)
+		conexao_redis.ping()
+		print("Conexao com o redis estabelecida")
+		worker = Worker(['operacoes'], connection=conexao_redis)
+		print("[*] Worker online e pronto para trabalhar..")
+		worker.work()
+	
+	except Exception as e:
+			print(f"Erro: {e}")
 
-fi __name__ == "__main__":
-	iniciar_worker()
+
+if __name__ == "__main__":
+	iniciar()
