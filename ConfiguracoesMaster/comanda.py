1230fbb60e33ca1f6b9d333669b3a9a8ae1e80f1
Andre Kenzo Naves Saijo <andre.saijo@sou.ufmt.br>
2026-05-06 19:57:21 -0400

    Update comanda.py

--- ConfiguracoesMaster/comanda.py
+++ ConfiguracoesMaster/comanda.py
@@ -3,11 +3,11 @@ import numpy as np
 
 @tarefa_cluster
 def processar_matriz_gigante(n):
-    # O código que você quiser!
+    # Qualquer codigo, qualquer
     data = np.random.rand(n, n)
     return np.linalg.inv(data) # Inversa de uma matriz
 
-# --- PARA MANDAR PROS WORKERS ---
+# --- MANDAR PROS WORKERS ---
 print("Enviando...")
 # Em vez de chamar a função direto, você usa .delay()
 meu_job = processar_matriz_gigante.delay(2000) 
