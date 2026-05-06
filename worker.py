b78b912db53ed70520bf4345c0f395c8c81c8e28
Andre Kenzo Naves Saijo <andre.saijo@sou.ufmt.br>
2026-05-06 19:43:59 -0400

    Update worker connection to Redis and refactor code
    Refactor worker initialization and connection handling.

--- worker.py
+++ worker.py
@@ -2,37 +2,20 @@ import redis
 from rq import Worker, Queue
 
 #Defina o ip e a porta do computador do redis
-#REDIS_IP = '10.10.10.10'
-#REDIS_PORT = 6379
+
 REDIS_URL = 'redis://:liama@10.10.10.10:6379'
 REDIS_IP ='10.10.10.10'
 
-#def iniciar_worker():
-#	#Configura a conexão	
-#	try:
-#		conexao = redis.from_url(REDIS_URL)
-#		
-#		conexao.ping()
-#		#Conecta na fila
-#		with Connection(conexao):
-#			worker = Worker(['operacoes'])
-#			print(f"[*] Conectado ao Redis em {REDIS_IP}")
-#			print(f"[*] Aguardando ordens")
-#			#Começa os trabalhos
-#			#O computador entrará em loop e estará constantemente vendo se há trabalhos
-#			worker.work()
-#	except redis.exceptions.AuthenticationError:
-#		print("Erro: Senha do Redis incorreta.")
-#	except Exception as e:
-#			print(f"[ERRO]: {e}")
-
 def iniciar():
 	try:
+		#Conectar ao Redis
 		conexao_redis = redis.from_url(REDIS_URL)
 		conexao_redis.ping()
 		print("Conexao com o redis estabelecida")
+		#Entra dentro da fila onde as funçoes serao enviadas
 		worker = Worker(['operacoes'], connection=conexao_redis, serializer=cloudpickle)
 		print("[*] Worker online e pronto para trabalhar..")
+		#loop onde o worker ira infinitamente vigiar o redis
 		worker.work()
 	
 	except Exception as e:
